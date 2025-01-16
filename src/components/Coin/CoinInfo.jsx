0ab83fd78b959f5a5338ad12ae97b9738558e56f
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-01-17 00:17:18 +0530

    changes

--- src/components/Coin/CoinInfo.jsx
+++ src/components/Coin/CoinInfo.jsx
@@ -4,8 +4,8 @@ import "../Dashboard/style.css"
 const CoinInfo = ({ heading, description }) => {
   return (
     <div className='block w-[95vw] rounded-3xl bg-[var(--darkgrey)] my-5 mx-auto break-words py-4 px-6 text-left'>
-      <h2 className='text-3xl font-semibold'>{heading}</h2>
-      <p className='highlight-a pt-3' dangerouslySetInnerHTML={{ __html: description }}></p>
+      <h2 className=' text-2xl md:text-3xl font-semibold'>{heading}</h2>
+      <p className='highlight-a pt-3 text-xs sm:text-sm md:text-base' dangerouslySetInnerHTML={{ __html: description }}></p>
     </div>
   );
 };
