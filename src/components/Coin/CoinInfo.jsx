2e46113ed5a5adf2312b9d39707a71c7df45d282
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-01-17 00:45:53 +0530

    changes

--- src/components/Coin/CoinInfo.jsx
+++ src/components/Coin/CoinInfo.jsx
@@ -5,6 +5,7 @@ const CoinInfo = ({ heading, description }) => {
 
    const shortDesc = description.slice(0,400) + "<span style='color:var(--grey); cursor:pointer;' > Read More...</span>"
    const longDesc = description  + "<span style='color:var(--grey); cursor:pointer;' > Show Less...</span>"
+   const longDesc2 = description 
    const [isExpanded, setIsExpanded] = useState(true);
   return (
     <div className='block w-[95vw] rounded-3xl bg-[var(--darkgrey)] my-5 mx-auto break-words py-4 px-6 text-justify tracking-tight'>
@@ -16,7 +17,7 @@ const CoinInfo = ({ heading, description }) => {
        ></p>:<p 
        onClick={()=>setIsExpanded(!isExpanded)}
        className={`highlight-a pt-3 text-xs sm:text-sm md:text-base ${!isExpanded && "cursor-pointer"}`}
-        dangerouslySetInnerHTML={{ __html: longDesc }}
+        dangerouslySetInnerHTML={{ __html: longDesc2 }}
         ></p>}
     </div>
   );
