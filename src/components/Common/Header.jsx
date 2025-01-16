777be3ad241a8453e491513bcb1968f57923db45
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-01-17 01:12:26 +0530

    changes

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -7,7 +7,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="flex justify-between items-center py-6 px-12 bg-[var(--black)] sticky top-0 left-0 z-50">
-      <h1 className="relative left-[-35px] md:left-0 lg:left-0 text-3xl md:text-3xl lg:text-3xl">CryptoTracker<span className="text-[var(--blue)]">.</span></h1>
+       <NavLink to={`/dashboard`} ><h1 className="relative left-[-35px] md:left-0 lg:left-0 text-3xl md:text-3xl lg:text-3xl">CryptoTracker<span className="text-[var(--blue)]">.</span></h1></NavLink>
 <div>
 <div className="hidden justify-end items-center gap-6 md:flex lg:flex ">
         <NavLink to="/"  className={(e)=>e.isActive?"font-semibold text-[var(--white)] hover:text-[var(--white)] hover:transition-all": "font-semibold text-[var(--grey)] hover:text-[var(--white)] hover:transition-all"}><p>Home</p></NavLink>
