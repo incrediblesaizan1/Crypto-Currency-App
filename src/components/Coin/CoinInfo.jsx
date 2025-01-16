fea0ad1b6fb16bb6f010c1745c90fbc32a4dedd2
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-01-17 00:31:26 +0530

    changes

--- src/components/Coin/CoinInfo.jsx
+++ src/components/Coin/CoinInfo.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../Dashboard/style.css"
 
 const CoinInfo = ({ heading, description }) => {
+
+   const shortDesc = description.slice(0,400) + "<span style='color:var(--grey); cursor:pointer;' > Read More...</span>"
+   const longDesc = description  + "<span style='color:var(--grey); cursor:pointer;' > Show Less...</span>"
+   const [isExpanded, setIsExpanded] = useState(true);
   return (
     <div className='block w-[95vw] rounded-3xl bg-[var(--darkgrey)] my-5 mx-auto break-words py-4 px-6 text-left'>
       <h2 className=' text-2xl md:text-3xl font-semibold'>{heading}</h2>
-      <p className='highlight-a pt-3 text-xs sm:text-sm md:text-base' dangerouslySetInnerHTML={{ __html: description }}></p>
+      <p 
+      onClick={()=>setIsExpanded(!isExpanded)}
+      className='highlight-a pt-3 text-xs sm:text-sm md:text-base'
+       dangerouslySetInnerHTML={{ __html: isExpanded?shortDesc:longDesc }}
+       ></p>
     </div>
   );
 };
